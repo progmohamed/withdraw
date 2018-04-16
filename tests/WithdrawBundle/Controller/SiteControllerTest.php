15bf55e3da9b9a32fb141f83da55c40caf90a0f7
m.nasser <m.nasser@smartech.my>
2018-04-16 17:49:16 +0400

    WIP unit test

--- tests/WithdrawBundle/Controller/SiteControllerTest.php
+++ tests/WithdrawBundle/Controller/SiteControllerTest.php
@@ -3,13 +3,23 @@
 namespace WithdrawBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class SiteControllerTest extends WebTestCase
 {
+
+
     public function testIndex()
     {
         $client = static::createClient();
-        $client->request('GET', '/en/admin/login');
+        $client->request('GET', '/en/admin/withdraw/site/show/2', array(), array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW'   => 'admin',
+        ));
+
         $this->assertTrue($client->getResponse()->isSuccessful());
+
     }
 }
