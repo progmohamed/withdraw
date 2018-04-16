84d45bd2060644352004d96f1242fa6d7570274d
mohamed nasser <m.nasser@smartech.my>
2018-04-16 20:54:20 +0400

    WIP TestCases

--- tests/WithdrawBundle/Controller/SiteControllerTest.php
+++ tests/WithdrawBundle/Controller/SiteControllerTest.php
@@ -3,23 +3,62 @@
 namespace WithdrawBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\BrowserKit\Cookie;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class SiteControllerTest extends WebTestCase
 {
+    private $em;
+    private $client;
+
+    public function setUp()
+    {
+        self::bootKernel();
+        $this->em = self::$kernel->getContainer()->get('doctrine')->getManager();
+
+        $this->client = static::createClient();
+        $this->client->setServerParameters(['PHP_AUTH_USER' => 'admin', 'PHP_AUTH_PW' => 'admin']);
+    }
 
 
     public function testIndex()
     {
-        $client = static::createClient();
-        $client->request('GET', '/en/admin/withdraw/site/show/2', array(), array(), array(
-            'PHP_AUTH_USER' => 'admin',
-            'PHP_AUTH_PW'   => 'admin',
-        ));
-
-        $this->assertTrue($client->getResponse()->isSuccessful());
-
+        $this->client->request('GET', '/en/admin/withdraw/site/');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
+
+
+    public function testNew()
+    {
+        $this->client->request('POST', '/en/admin/withdraw/site/new', array('url' => ['sss', 'mmm']));
+        $this->assertTrue($this->client->getResponse()->isRedirect('/en/admin/withdraw/site/'));
+    }
+
+
+    public function testEdit()
+    {
+
+        $crawler = $this->client->request('GET', '/en/admin/withdraw/site/edit/1');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+
+        $form = $crawler->selectButton('submit')->form(array(
+            'site[url]' => 'testtesttest',
+        ));
+        $this->client->submit($form);
+        $this->assertTrue($this->client->getResponse()->isRedirect('/en/admin/withdraw/site/show/1'));
+    }
+
+
+    public function testShow()
+    {
+        $this->client->request('GET', '/en/admin/withdraw/site/show/1');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+    }
+
+    public function testDelete()
+    {
+        $this->client->request('POST', '/en/admin/withdraw/site/delete?id=' . base64_encode(serialize([2])));
+        $this->assertTrue($this->client->getResponse()->isRedirect('/en/admin/withdraw/site/'));
+    }
+
+
 }
